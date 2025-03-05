13c531062a4ddec107f3c0c2d65f8a18d73db973
Bruno Carvalho <61797027+BrunixD@users.noreply.github.com>
2025-03-05 16:18:09 +0000

    Separate In Out tabs

--- LCM_Frontend/src/components/Layout/Tabs/ViewModeTabs.jsx
+++ LCM_Frontend/src/components/Layout/Tabs/ViewModeTabs.jsx
@@ -1,4 +1,3 @@
-// src/components/Layout/ViewModeTabs.jsx
 import React, { useState, useEffect } from 'react';
 import Tab from './Tab';
 import Table from '../Tables/Table';
@@ -10,48 +9,49 @@ import CompetitionViewTabs from './CompetitionViewTabs';
 import useData from '../../hooks/useData';
 
 const ViewModeTabs = () => {
-  const [activeTab, setActiveTab] = useState('Atletas');
-  const [isMerchandiseTab, setIsMerchandiseTab] = useState(false);
-  const { data, loading, error, fetchData } = useData(activeTab);
+    const [activeTab, setActiveTab] = useState('Atletas');
+    const [isMerchandiseTab, setIsMerchandiseTab] = useState(false);
+    const { data, loading, error, fetchData } = useData();  // Remove initialTab here.
 
-  useEffect(() => {
-    fetchData(activeTab);
-    setIsMerchandiseTab(activeTab === 'Merchandise');
-  }, [activeTab, fetchData]);
+    useEffect(() => {
+      fetchData(activeTab);
+      setIsMerchandiseTab(activeTab === 'Merchandise');
+    }, [activeTab, fetchData]);
 
-  return (
-    <div className="view-mode">
-      <div className="tab-container">
-        <Tab label="Athletes" isActive={activeTab === 'Atletas'} onClick={() => setActiveTab('Atletas')} />
-        <Tab label="Monthly Payments" isActive={activeTab === 'Mensalidade'} onClick={() => setActiveTab('Mensalidade')} />
-        <Tab label="Merchandise" isActive={activeTab === 'Merchandise'} onClick={() => setActiveTab('Merchandise')} />
-        <Tab label="All Payments" isActive={activeTab === 'Todos_Pagamentos'} onClick={() => setActiveTab('Todos_Pagamentos')} />
-        <Tab label="Competition" isActive={activeTab === 'Competicao'} onClick={() => setActiveTab('Competicao')} /> {/* Add Competition Tab */}
-        <Tab label="Profit" isActive={activeTab === 'profit'} onClick={() => setActiveTab('profit')} />
-        <Tab label="IN/OUT" isActive={activeTab === 'Entrada_Saida'} onClick={() => setActiveTab('Entrada_Saida')} />
-      </div>
+    return (
+        <div className="view-mode">
+            <div className="tab-container">
+                <Tab label="Athletes" isActive={activeTab === 'Atletas'} onClick={() => setActiveTab('Atletas')} />
+                <Tab label="Monthly Payments" isActive={activeTab === 'Mensalidade'} onClick={() => setActiveTab('Mensalidade')} />
+                <Tab label="Merchandise" isActive={activeTab === 'Merchandise'} onClick={() => setActiveTab('Merchandise')} />
+                <Tab label="All Payments" isActive={activeTab === 'Todos_Pagamentos'} onClick={() => setActiveTab('Todos_Pagamentos')} />
+                <Tab label="Competition" isActive={activeTab === 'Competicao'} onClick={() => setActiveTab('Competicao')} />
+                <Tab label="Profit" isActive={activeTab === 'profit'} onClick={() => setActiveTab('profit')} />
+                <Tab label="In Values" isActive={activeTab === 'Entrada_Valores'} onClick={() => setActiveTab('Entrada_Valores')} />
+                <Tab label="Out Values" isActive={activeTab === 'Saida_Valores'} onClick={() => setActiveTab('Saida_Valores')} />
+            </div>
 
-      <div className="content-container">
-        {loading ? (
-          <p>Loading...</p>
-        ) : error ? (
-          <p className="error">Error: {error}</p>
-        ) : activeTab === 'Atletas' || activeTab === 'Profit' ? (
-          <Table data={data} tableName={activeTab} />
-        ) : activeTab === 'Mensalidade' ? (
-          <ViewModeMonthlySubTabs />
-        ) : activeTab === 'Merchandise' ? (
-          <ViewModeMerchandiseTabs />
-        ) : activeTab === 'Todos_Pagamentos' ? (
-          <TodosPagamentosTable data={data} />
-        ) : (activeTab === 'Competicao')? (
-          <CompetitionViewTabs/>
-        ) : (activeTab === 'Entrada_Saida')? (
-          <InOutViewTabs/>
-        ): null}
-      </div>
-    </div>
-  );
+            <div className="content-container">
+                {loading ? (
+                    <p>Loading...</p>
+                ) : error ? (
+                    <p className="error">Error: {error}</p>
+                ) : activeTab === 'Atletas' || activeTab === 'Profit' ? (
+                    <Table data={data} tableName={activeTab} />
+                ) : activeTab === 'Mensalidade' ? (
+                    <ViewModeMonthlySubTabs />
+                ) : activeTab === 'Merchandise' ? (
+                    <ViewModeMerchandiseTabs />
+                ) : activeTab === 'Todos_Pagamentos' ? (
+                    <TodosPagamentosTable data={data} />
+                ) : (activeTab === 'Competicao') ? (
+                    <CompetitionViewTabs />
+                ) : (activeTab === 'Entrada_Valores' || activeTab === 'Saida_Valores') ? (
+                    <Table data={data} tableName={activeTab}/>
+                ) : null}
+            </div>
+        </div>
+    );
 };
 
 export default ViewModeTabs;
--- LCM_Frontend/src/components/hooks/useData.js
+++ LCM_Frontend/src/components/hooks/useData.js
@@ -1,67 +1,76 @@
-// hooks/useData.js
 import { useState, useCallback } from 'react';
 import { supabase } from '../../createClient';
 
-const useData = (initialTab) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+const useData = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
-  const fetchData = useCallback(async (tableName) => {
-    setLoading(true);
-    setError(null);
-    try {
-      let query = supabase.from(tableName).select('*');
+    const fetchData = useCallback(async (tableName) => {
+        setLoading(true);
+        setError(null);
+        try {
+            let query = supabase.from(tableName).select('*');
 
-      if (tableName === 'Atletas') {
-        query = supabase.from('Atletas').select('*, equipa (nome)');
-      }
+            if (tableName === 'Atletas') {
+                query = supabase.from('Atletas').select('*, equipa (nome)');
+            }
 
-      if (tableName === 'Merchandise') {
-        query = supabase.from('Merchandise').select('*');
-      }
+            if (tableName === 'Merchandise') {
+                query = supabase.from('Merchandise').select('*');
+            }
 
-      if (tableName === 'Merchandise_Comprado') {
-        query = supabase
-          .from('Merchandise_Comprado')
-          .select(`
+            if (tableName === 'Merchandise_Comprado') {
+                query = supabase
+                    .from('Merchandise_Comprado')
+                    .select(`
             id,
             quantidade,
             Merchandise (nome),
             Atletas (nome)
           `);
-      }
+            }
 
-      if (tableName === 'Todos_Pagamentos') {
-        fetchAllPaymentsData();
-        return;
-      }
+            if (tableName === 'Entrada_Valores') {
+                query = supabase
+                    .from('Entrada_Valores')
+                    .select('*');
+            }
 
-      if (tableName === 'Entrada_Saida') {
-        return;
-      }
+            if (tableName === 'Saida_Valores') {
+                query = supabase
+                    .from('Saida_Valores')
+                    .select('*');
+            }
 
-      const { data, error } = await query;
-      if (error) {
-        throw error;
-      }
-      setData(data || []);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+            if (tableName === 'Todos_Pagamentos') {
+                fetchAllPaymentsData();
+                return;
+            }
 
-  const fetchAllPaymentsData = useCallback(async () => {
-    setLoading(true);
-    setError(null);
+            const { data, error } = await query;
+            if (error) {
+                throw error;
+            }
+            setData(data || []);
+            return data || []; // Return data
+        } catch (error) {
+            setError(error.message);
+            return null; // Return null in case of error
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
-    try {
-      // Fetch all data sources separately
-      const { data: todosPagamentosData, error: todosPagamentosError } = await supabase
-        .from('Todos_Pagamentos')
-        .select(`
+    const fetchAllPaymentsData = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+
+        try {
+            // Fetch all data sources separately
+            const { data: todosPagamentosData, error: todosPagamentosError } = await supabase
+                .from('Todos_Pagamentos')
+                .select(`
           *,
           Atletas (nome),
           Merchandise_Comprado (
@@ -71,49 +80,49 @@ const useData = (initialTab) => {
             )
         `);
 
-      if (todosPagamentosError) throw todosPagamentosError;
+            if (todosPagamentosError) throw todosPagamentosError;
 
-      const { data: mensalidadeData, error: mensalidadeError } = await supabase
-        .from('Mensalidade')
-        .select(`
+            const { data: mensalidadeData, error: mensalidadeError } = await supabase
+                .from('Mensalidade')
+                .select(`
           *,
           Atletas (nome, equipa (valor_mensalidade))
         `);
 
-      if (mensalidadeError) throw mensalidadeError;
+            if (mensalidadeError) throw mensalidadeError;
 
-      const { data: torneiosData, error: torneiosError } = await supabase
-        .from('Atletas_pagamento_torneios')
-        .select(`
+            const { data: torneiosData, error: torneiosError } = await supabase
+                .from('Atletas_pagamento_torneios')
+                .select(`
           *,
           Atletas (nome),
           Competicao (torneio, valor_individual, valor_equipa)
         `);
 
-      if (torneiosError) throw torneiosError;
+            if (torneiosError) throw torneiosError;
 
-      // Combine and transform data
-      const combinedData = [
-        ...(todosPagamentosData || []),  // Keep "other" payments
-        ...(mensalidadeData || []).map(item => ({
-          ...item,
-          tipo_pagamento: 'Mensalidade',
-        })),
-        ...(torneiosData || []).map(item => ({
-          ...item,
-          tipo_pagamento: 'Torneio',
-        })),
-      ];
+            // Combine and transform data
+            const combinedData = [
+                ...(todosPagamentosData || []),  // Keep "other" payments
+                ...(mensalidadeData || []).map(item => ({
+                    ...item,
+                    tipo_pagamento: 'Mensalidade',
+                })),
+                ...(torneiosData || []).map(item => ({
+                    ...item,
+                    tipo_pagamento: 'Torneio',
+                })),
+            ];
 
-      setData(combinedData);
-    } catch (error) {
-      setError(error.message);
-    } finally {
-      setLoading(false);
-    }
-  }, []);
+            setData(combinedData);
+        } catch (error) {
+            setError(error.message);
+        } finally {
+            setLoading(false);
+        }
+    }, []);
 
-  return { data, loading, error, fetchData };
+    return { data, loading, error, fetchData };
 };
 
 export default useData;
